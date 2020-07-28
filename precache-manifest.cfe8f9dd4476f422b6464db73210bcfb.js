a40cfcd672765d9ba96477848ce138d1466b0f65
Obbik <norbertdering@gmail.com>
2020-07-28 15:04:47 +0200

    Updates

---
+++ precache-manifest.cfe8f9dd4476f422b6464db73210bcfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896e75c6a144ddae4cc4df78cfca937a",
+    "url": "/React-todoapp/index.html"
+  },
+  {
+    "revision": "de6a80bae6236aeba8b8",
+    "url": "/React-todoapp/static/css/main.c5a1c8d6.chunk.css"
+  },
+  {
+    "revision": "4fe9053fb52338eb2a86",
+    "url": "/React-todoapp/static/js/2.2f255bd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todoapp/static/js/2.2f255bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6a80bae6236aeba8b8",
+    "url": "/React-todoapp/static/js/main.655a1d7f.chunk.js"
+  },
+  {
+    "revision": "6db36fdbc198832dd7ed",
+    "url": "/React-todoapp/static/js/runtime-main.55cc84ca.js"
+  }
+]);
